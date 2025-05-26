065dbf20aa2bb40ebde9442bcb257f0b0000650a
Stephen <shly@student.unimelb.edu.au>
2025-05-26 19:49:37 +1000

    HTML pages now link to one another. Users can navigate around website

---
+++ JavaScript/links.js
@@ -0,0 +1,39 @@
+// Takes users to Browsing Page from the Home Page
+function goToBrowse1() {
+  window.location.href = "HTML/BrowsingPage.html";
+}
+
+// Takes users to Browsing Page from all other pages
+function goToBrowse2() {
+  window.location.href = "BrowsingPage.html";
+}
+
+// Takes users to Product Page that displays Black Sweatershirt
+function goToProduct1() {
+  window.location.href = "ProductPage1.html";
+}
+
+// Takes users to Product Page that displays Air Jordan
+function goToProduct2() {
+  window.location.href = "ProductPage2.html";
+}
+
+// Takes users to Home Page
+function goToHome() {
+  window.location.href = "../index.html";
+}
+
+// Takes users to Shopping Cart Page
+function goToCart() {
+  window.location.href = "ShoppingCartPage.html";
+}
+
+// Takes users to Payment Page
+function goToPay() {
+  window.location.href = "PaymentPage.html";
+}
+
+// Takes users to Confirmation Page
+function goToConfirm() {
+  window.location.href = "ConfirmationPage.html";
+}
